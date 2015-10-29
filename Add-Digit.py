e235e379e2409dcc60f461b1d4452757f5a6dfe4
Xiaoxiami <chang-xiao-dong@hotmail.com>
2015-10-29 23:31:14 +0800

    10-29

---
+++ Add-Digit.py
@@ -0,0 +1,12 @@
+__author__ = 'Changxiaodong'
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        return (-1)%9
+
+if __name__ == "__main__":
+    test = Solution()
+    print test.addDigits(0)
