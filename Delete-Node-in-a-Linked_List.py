204f23461305e4f0124a4b6162bffd72fab3f293
Xiaoxiami <chang-xiao-dong@hotmail.com>
2015-10-29 23:38:31 +0800

    10-29

--- Delete-Node-in-a-Linked_List.py
+++ Delete-Node-in-a-Linked_List.py
@@ -1,9 +1,10 @@
 __author__ = 'Changxiaodong'
+import time
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def deleteNode(self, node):
---
+++ template.py
@@ -0,0 +1,7 @@
+__author__ = 'Changxiaodong'
+import time
+
+if __name__ == "__main__":
+    start = time.clock()
+
+    print time.clock() - start
