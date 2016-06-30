f30b2511cb978009ab767fff9b2425c5cf66de9b
xiaoxiami <changxiaodong.cxd@gmail.com>
2016-06-30 18:59:18 +0800

    fit for python3

--- 9-Palindrome-Number.py
+++ 9-Palindrome-Number.py
@@ -12,38 +12,42 @@ You could also try reversing an integer. However, if you have solved the problem
 
 There is a more generic way of solving this problem.
 '''
+
+
 def isPalindrome(x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        if x<0 or (x%10 == 0 and x > 0):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0 or (x % 10 == 0 and x > 0):
+        return False
+    length = len("%d" % x)
+    for i in range(length // 2):
+        if x % 10 == 0:
+            x = x // 10
+            continue
+        first = x // pow(10, length - i * 2 - 1)
+        last = x % 10
+        if first != last:
             return False
-        length = len("%d" % x)
-        for i in range(int(length/2)):
-            if x % 10 == 0:
-                x = int(x/10)
-                continue
-            first = int(x/pow(10,length-i*2-1))
-            last = x%10
-            if first != last:
-                return False
-            x = x - first*pow(10,length-i*2-1)
-            x = int(x/10)
-        return True
+        x = x - first * pow(10, length - i * 2 - 1)
+        x = x // 10
+    return True
+
 
 def isPalindrome1(x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        if x<0:
-            return False
-        normal = x
-        reverse = 0
-        while x > 0:
-            reverse = reverse * 10 + x%10
-            x =int(x / 10)
-        return reverse == normal
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0:
+        return False
+    normal = x
+    reverse = 0
+    while x > 0:
+        reverse = reverse * 10 + x % 10
+        x = int(x / 10)
+    return reverse == normal
 
-print(isPalindrome1(122321))
+
+print(isPalindrome1(12321))
