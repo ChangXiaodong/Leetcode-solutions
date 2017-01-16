c2752e6d0220367c8d51f30761d6a934c0efc73a
ChangXiaodong <changxiaodong.cxd@gmail.com>
2017-01-16 22:09:51 +0800

    Triangle

--- 120-Triangle.py
+++ 120-Triangle.py
@@ -12,12 +12,34 @@ def minimumTotal(triangle):
                 triangle_steps[row - 1].append(v + min(triangle_steps[row][i], triangle_steps[row][i + 1]))
         else:
             triangle_steps[row - 1] = triangle[row - 1]
-    return triangle_steps
+    return triangle_steps[0][0]
+
+def minimumTotal1(triangle):
+    if not triangle:
+        return
+    res = [[0 for i in range(len(row))] for row in triangle]
+    res[0][0] = triangle[0][0]
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                res[i][j] = res[i-1][j] + triangle[i][j]
+            elif j == len(triangle[i])-1:
+                res[i][j] = res[i-1][j-1] + triangle[i][j]
+            else:
+                res[i][j] = min(res[i-1][j-1], res[i-1][j]) + triangle[i][j]
+    return min(res[-1])
 
 
-minimumTotal([
+print(minimumTotal([
     [2],
     [3, 4],
     [6, 5, 7],
     [4, 1, 8, 3]
-])
+]))
+
+print(minimumTotal1([
+    [2],
+    [3, 4],
+    [6, 5, 7],
+    [4, 1, 8, 3]
+]))
