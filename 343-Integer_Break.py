4ca50cf28143be080cb5f14430fd46c1160f1ce4
xiaoxiami-mac <changxiaodonh.cxd@mac>
2017-01-17 23:42:25 +0800

    343.Interger Break

--- 343-Integer_Break.py
+++ 343-Integer_Break.py
@@ -17,4 +17,21 @@ def integerBreak(n):
         return pow(3, numof3) * 3
 
 
+def integerBreak1(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    if n == 2:
+        return 1
+    if n == 3:
+        return 2
+    product = 1
+    while n > 4:
+        product *= 3
+        n -= 3
+    product *= n
+    return product
+
 print(integerBreak(10))
+print(integerBreak1(10))
