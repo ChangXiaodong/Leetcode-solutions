24acf465d387d95907fe357aa1d626d847c58c58
ChangXiaodong <changxiaodong.cxd@gmail.com>
2017-02-23 16:36:27 +0800

    81. Search in Rotated Sorted Array

--- 81-Search_in_Rotated_Sorted_Array.py
+++ 81-Search_in_Rotated_Sorted_Array.py
@@ -1,3 +1,14 @@
+'''
+与没有重复的情况不同的地方在于不确定什么时候更新low和high，没有办法准确找出rotate点。
+有两种办法：
+1> 用O(n)时间遍历数组，找到rotate点，再用二分查找
+2> 平均复杂度O(log(n))，最坏复杂度O(n),在查找过程中选择更新low和high的方法。
+把一个rotated数组看成两部分，大端和小端。如[4 5 6 7 0 1 2]， 大端为[4 5 6 7]，小端为[0 1 2]
+首先判断middle实在大端部分还是小端部分，如果在大端部分，在判断target是不是落在大端，根据结果更新low和high，若middle在
+小端，判断target是否落在小端，并更新low和high。
+如果直到lo==high都没有找到target，则target不存在。
+
+'''
 def search(nums, target):
     """
     :type nums: List[int]
