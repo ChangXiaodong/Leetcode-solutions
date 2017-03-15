a2743fa93989b26499a103da7c599af6239b5613
ChangXiaodong <changxiaodong.cxd@gmail.com>
2017-03-15 09:43:43 +0800

    153. Find Minimum in Rotated Sorted Array

---
+++ 2/153-Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+'''
+二分查找，注意更新low和high的条件
+'''
+def findMin(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if not nums:
+        return
+    low = 0
+    high = nums.__len__() - 1
+    while low < high:
+        middle = (low + high) // 2
+        if nums[middle] > nums[high]:
+            low = middle + 1
+        else:
+            high = middle
+    return nums[low]
