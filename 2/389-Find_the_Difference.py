37b97290096dfc740d05e0337c708b3d5f3f3eed
ChangXiaodong <changxiaodong.cxd@gmail.com>
2017-03-20 20:34:21 +0800

    389. Find the Difference

---
+++ 2/389-Find_the_Difference.py
@@ -0,0 +1,30 @@
+'''
+方法1：用哈希表
+方法2：异或
+'''class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        hash_map = {}
+        for v in s:
+            hash_map[v] = hash_map.get(v, 0) + 1
+        for v in t:
+            if hash_map.get(v, "Null") == "Null" or hash_map.get(v, "Null") == 0:
+                return v
+            hash_map[v] -= 1
+
+    def findTheDifference1(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        res = 0
+        for v in s:
+            res = res ^ ord(v)
+        for v in t:
+            res = res ^ ord(v)
+        return chr(res)
