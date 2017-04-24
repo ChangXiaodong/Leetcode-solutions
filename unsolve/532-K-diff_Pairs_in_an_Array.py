cca5bf75f33b9254255a1e0c224a956352773882
ChangXiaodong <changxiaodong.cxd@gmail.com>
2017-04-24 08:59:25 +0800

    532. K-diff Pairs in an Array

--- unsolve/532-K-diff_Pairs_in_an_Array.py
+++ unsolve/532-K-diff_Pairs_in_an_Array.py
@@ -1,3 +1,10 @@
+# coding=utf-8
+'''
+方法1：哈希表，注意k为0 的时候
+方法2：双指针
+每次j从i+1开始遍历，找到nums[j]不小于nums[i]的数， 看num[i],num[j]的差是否等于k
+然后把i向前挪， 挪到下个不重复的数字重新开始遍历。
+'''
 class Solution(object):
     def findPairs(self, nums, k):
         """
@@ -8,24 +15,35 @@ class Solution(object):
         if k < 0 or len(nums) < 2:
             return 0
         res = 0
-        index1 = 0
-        index2 = 1
-        nums.sort()
-        while index2 < len(nums):
-            if nums[index2] - nums[index1] < k:
-                index2 += 1
-            elif nums[index2] - nums[index1] > k:
-                index1 += 1
+
+        table = {}
+        for v in nums:
+            table[v] = table.get(v, 0) + 1
+        for key, v in table.items():
+            if k == 0:
+                if v >= 2:
+                    res += 1
             else:
-                res += 1
-                index1 += 1
-                index2 += 1
-                while index1 < len(nums) - 1 and nums[index1] == nums[index1 + 1]:
-                    index1 += 1
-                    index2 += 1
-            if index1 == index2:
-                index2 += 1
+                if table.get(key + k, "null") != "null":
+                    res += 1
         return res
 
+    def findPairs1(self, nums, k):
+        nums.sort()
+        i = 0
+        j = 0
+        res = 0
+        while i < len(nums):
+            j = max(j, i + 1)
+            while j < len(nums) and nums[j] - nums[i] < k:
+                j += 1
+            if j < len(nums) and nums[j] - nums[i] == k:
+                res += 1
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            i += 1
+        return res
+
+
 solution = Solution()
-print(solution.findPairs([6,3,5,7,2,3,3,8,2,4], 2))
+print(solution.findPairs1([3, 1, 4, 4, 5], 2))
