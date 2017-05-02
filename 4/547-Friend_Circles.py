7acf67ac9bcca43dbcc23bac7dcdf28c5d862b0c
xiaoxiami-mac <changxiaodonh.cxd@mac>
2017-05-03 00:20:19 +0800

    bfs and dfs

--- 4/547-Friend_Circles.py
+++ 4/547-Friend_Circles.py
@@ -5,6 +5,8 @@
 一个marked数组存储当前节点是否被访问过，如果被访问过则不能进行第二次访问。
 一个队列，quene，用来报保存与当前节点连通的节点。初始化时把起始节点推入队列中。
 '''
+
+
 class Solution(object):
     def findCircleNum(self, M):
         """
@@ -12,48 +14,57 @@ class Solution(object):
         :rtype: int
         """
         nums = len(M)
-        buf = []
+        res = []
         marked = [False for _ in range(nums)]
         for j in range(nums):
             if marked[j]:
                 continue
             marked[j] = True
-            buf.append([j])
+            res.append([j])
             stack = [j]
             while stack:
                 s = stack.pop(0)
                 for i in range(nums):
-                    if s != i and M[s][i] == 1 and i not in buf[-1]:
+                    if s != i and M[s][i] == 1 and i not in res[-1]:
                         marked[i] = True
                         stack.append(i)
-                        buf[-1].append(i)
-        return len(buf)
+                        res[-1].append(i)
+        return len(res)
 
-    def dfs(self, M, buf, res, index, visited):
-        if M[]
-        if not visited[j]
-
-
+    def dfs(self, M, buf, index, visited):
+        for i in range(len(M)):
+            if i != index and M[i][index] and not visited[i]:
+                visited[i] = True
+                buf.append(i)
+                buf = self.dfs(M, buf, i, visited)
+        return buf
 
     def findCircleNum1(self, M):
-        pass
+        res = []
+        visited = [False for _ in range(len(M))]
+        for i in range(len(M)):
+            if not visited[i]:
+                buf = [i]
+                visited[i] = True
+                res.append(self.dfs(M, buf, i, visited))
+        return len(res)
 
 
 solution = Solution()
-print(solution.findCircleNum(
-    [[1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-     [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
-    ))
+num = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+       [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+       [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+       [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+       [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+solution.findCircleNum(num)
+print(solution.findCircleNum1(num))
