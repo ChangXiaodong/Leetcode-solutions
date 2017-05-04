8d879d6f0d7fcd7dafeac4566d6e61261e2ea203
ChangXiaodong <changxiaodong.cxd@gmail.com>
2017-05-04 16:56:16 +0800

    堆排序

--- Introduction_to_algorithm/section_6/heap_sort.py
+++ Introduction_to_algorithm/section_6/heap_sort.py
@@ -35,12 +35,38 @@ def max_heap(ary, i, size):
         max_heap(ary, largest, size)
     return ary
 
+
 def swap(ary, i, j):
     ary[i], ary[j] = ary[j], ary[i]
 
 
+def sink(ary, k, N):
+    while 2 * k <= N:
+        j = 2 * k
+        if j < N and ary[j] < ary[j + 1]:
+            j += 1
+        if not ary[k] < ary[j]:
+            break
+        swap(ary, j, k)
+        k = j
+
+
+def heap_sort2(nums):
+    if not nums:
+        return []
+    n = nums.__len__() - 1
+    k = n / 2
+    while k >= 1:
+        sink(ary, k, n)
+        k -= 1
+    while n > 1:
+        swap(ary, 1, n)
+        n -= 1
+        sink(ary, 1, n)
+    return nums
+
 if __name__ == "__main__":
-    ary = [16, 4, 10, 14, 7, 9, 3, 2, 8, 100]
+    ary = [0, 16, 4, 10, 14, 7, 9, 3, 2, 8, 100]
     # [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
-    result = build_max_heap(ary)
-    print(result)
+
+    print(heap_sort2(ary))
