ee139ccb104d5e9ee99edcfa45de64ed4a5dcec6
ChangXiaodong <changxiaodong.cxd@gmail.com>
2017-05-30 22:56:52 +0800

    209

---
+++ 3/209-Minimum_Size_Subarray_Sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = left = 0
+        result = len(nums) + 1
+        for right, n in enumerate(nums):
+            total += n
+            while total >= s:
+                result = min(result, right - left + 1)
+                total -= nums[left]
+                left += 1
+        return result if result <= len(nums) else 0
+
+
+s = Solution()
+print(s.minSubArrayLen(11, [1, 1, 1, 1, 7]))
