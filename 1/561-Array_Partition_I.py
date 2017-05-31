695983833b3820ae1bc702eb081c45f45efd5520
xiaoxiami-mac <changxiaodonh.cxd@mac>
2017-05-31 22:11:16 +0800

    561. Array Partition I

---
+++ 1/561-Array_Partition_I.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        res = 0
+        for i in range(0, len(nums), 2):
+            res += nums[i]
+        return res
