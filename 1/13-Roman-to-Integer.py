5422f2ead7e4b0d12468c445e9041d6f8650c611
xiaoxiami-mac <changxiaodonh.cxd@mac>
2017-06-12 23:24:20 +0800

    18. 4Sum

--- 1/13-Roman-to-Integer.py
+++ 1/13-Roman-to-Integer.py
@@ -44,7 +44,6 @@ class Solution(object):
     def romaToInt1(self,s):
         romanDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, '0': 0}
         return sum([romanDict[c] if romanDict[c] >= romanDict[d] else romanDict[c]*-1 for c,d in zip(s, (s + '0')[1:])])
-        #上面这句话写成正常形式
         # num = 0
         # for c,d in zip(s, (s + '0')[1:]):
         #     if romanDict[c] >= romanDict[d]:
